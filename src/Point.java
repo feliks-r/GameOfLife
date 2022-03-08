05792fbb72af122d6f89e0a7199f997b23ba654b
feliks-r <92568407+feliks-r@users.noreply.github.com>
2022-03-08 19:47:44 +0100

    Initial commit

---
+++ src/Point.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Point {
+	private ArrayList<Point> neighbors;
+	private int currentState;
+	private int nextState;
+	private int numStates = 6;
+	Random rand = new Random();
+	
+	public Point() {
+		currentState = 0;
+		nextState = 0;
+		neighbors = new ArrayList<Point>();
+	}
+
+	public void clicked() {
+		currentState=(++currentState)%numStates;	
+	}
+	
+	public int getState() {
+		return currentState;
+	}
+
+	public void setState(int s) {
+		currentState = s;
+	}
+
+	public void calculateNewState() {
+		//TODO: insert logic which updates according to currentState and 
+		//number of active neighbors
+
+		int activeNeighbors = countActiveNeighbors();
+		if (activeNeighbors == 3 || (currentState == 1 && activeNeighbors == 2)){
+			nextState = 1;
+		}
+		else{
+			nextState = 0;
+		}
+	}
+
+	public void calculateRainState(){
+		if(currentState>0){
+			nextState=(--currentState)%numStates;
+		}
+		else if(!neighbors.isEmpty() && neighbors.get(0).getState()>0){
+			nextState = 6;
+		}
+	}
+
+	public void changeState() {
+		currentState = nextState;
+	}
+	
+	public void addNeighbor(Point nei) {
+		neighbors.add(nei);
+	}
+	
+	//TODO: write method counting all active neighbors of THIS point
+
+	public int countActiveNeighbors(){
+		int result = 0;
+		for (Point n : neighbors){
+			result+=n.currentState;
+		}
+		return result;
+	}
+
+	public void drop(){
+		int n = rand.nextInt(100);
+		if(n < 5){
+			nextState = 6;
+		}
+	}
+}
